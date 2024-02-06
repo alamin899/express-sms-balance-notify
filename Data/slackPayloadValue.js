4a928606861e5e16f6867e3fafcce56f16a51fae
alaminAppnap <alamin@appnap.io>
2024-02-06 08:58:42 +0600

    update payload value

--- Data/slackPayloadValue.js
+++ Data/slackPayloadValue.js
@@ -1,7 +1,5 @@
 const readyPayload = (data) => {
-    const phone = data && data.phone ? data.phone : "01700000000";
-    const message = data && data.message ? data.message : "Test message";
-
+    
     return {
   
         "attachments":[
@@ -9,18 +7,11 @@ const readyPayload = (data) => {
               "fallback":"Available SMS Balance",
               "pretext":"Available SMS Balance",
               "color":"#D00000",
-              "fields":[
-                 {
-                  "title":"Mobile",
-                  "value":phone,
-                  "short":false
-               },
-               {
-                "title":"Message",
-                "value":message,
-                "short":false
-             },
-              ]
+              "fields":data.map(item => ({
+                  "title": item.label,
+                  "value": item.balance,
+                  "short": false
+             }))
            }
         ]
      }
--- server.js
+++ server.js
@@ -15,70 +15,39 @@ const port = process.env.PORT || 3000;
 // Define your scheduled task to run every minute
 cron.schedule('*/1 * * * *', async () => {
   const slackWebhookUrl = process.env.SLACK_WEB_HOOK_URL;
-  const csmsIdsData = process.env.SSL_WIRELESS_CSMS_IDS;
   const apiToken = process.env.SSL_WIRELESS_API_TOKEN;
   const apiUrl = process.env.SSL_API_ENDPOINT;
 
+  const csmsIdsData = process.env.SSL_WIRELESS_CSMS_IDS;
   const csmsIds = csmsIdsData.split(',');
   const notificationMessages = [];
 
-  // const payload = {api_token:apiToken,sid:csmsIds[0]};
-
-  //       const response =await callApi(apiUrl, payload);
-  //       console.log(response.data);
-  //       sendNotification(slackWebhookUrl, {message:response.data.sid,phone:response.data.balance},readyPayload);
   if (csmsIds.length > 0) {
     for (const id of csmsIds) {
       const payload = {api_token:apiToken,sid:id};
-      console.log("--------",payload)
+      
       try {
         const response = await callApi(apiUrl, payload);
-        notificationMessages.push({
-          label:id,balance:response.data.balance
-        })
+        notificationMessages.push(
+          {
+            label:id,
+            balance:response.data.balance
+          }
+        )
       } catch (error) {
         console.error(`Error calling API for ID ${id}:`, error.message);
       }
     }
 
-    console.log(notificationMessages);
-    sendNotification(slackWebhookUrl, notificationMessages,readyPayload);
+    if(notificationMessages.length>0){
+      sendNotification(slackWebhookUrl, notificationMessages,readyPayload);
+    }
   }
 });
 
 
 
 app.get('/',async (req, res) => {
-  const slackWebhookUrl = process.env.SLACK_WEB_HOOK_URL;
-  const csmsIdsData = process.env.SSL_WIRELESS_CSMS_IDS;
-  const apiToken = process.env.SSL_WIRELESS_API_TOKEN;
-  const apiUrl = process.env.SSL_API_ENDPOINT;
-
-  const csmsIds = csmsIdsData.split(',');
-  const notificationMessages = [];
-
-  // const payload = {api_token:apiToken,sid:csmsIds[0]};
-
-  //       const response =await callApi(apiUrl, payload);
-  //       console.log(response.data);
-  //       sendNotification(slackWebhookUrl, {message:response.data.sid,phone:response.data.balance},readyPayload);
-  if (csmsIds.length > 0) {
-    for (const id of csmsIds) {
-      const payload = {api_token:apiToken,sid:id};
-      console.log("--------",payload)
-      try {
-        const response =await callApi(apiUrl, payload);
-        notificationMessages.push({
-          label:id,balance:response.data.balance
-        })
-      } catch (error) {
-        console.error(`Error calling API for ID ${id}:`, error.message);
-      }
-    }
-
-    console.log(notificationMessages);
-    sendNotification(slackWebhookUrl, notificationMessages,readyPayload);
-  }
   res.send(`Landing pages`);
 })
 
